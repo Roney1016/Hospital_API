d094aef69e528fa1bacfa745a58989db4a3c14ec
manish kumar ravi <manishkumarravi1016@gmail.com>
2023-10-11 15:39:18 +0530

    set views engine and set assets file and body parser

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//********************     Set Template Engine     ********************//
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, 'views'));
+
+
+//********************      Assets       ******************************/
+const assetsPath = path.join(__dirname, "assets");
+app.use(express.static(assetsPath));
+app.use(express.static(__dirname + '/assets'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+
+
+
 app.get('/', function (req, res) {
-    return res.send('hello')
+    return res.render('hospital')
 })
+
+
+
 
 app.listen(PORT, () => {
     console.log('server is running on port', PORT)
