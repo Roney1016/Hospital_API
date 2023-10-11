fe0cc4ac0226bee64c5b6dcf541c8cf28b7e9b08
manish kumar ravi <manishkumarravi1016@gmail.com>
2023-10-11 16:35:53 +0530

    create doctor schema

---
+++ models/doctorModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+//  doctor schema
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Doctor', doctorSchema)
